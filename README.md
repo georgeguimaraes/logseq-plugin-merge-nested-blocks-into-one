ebfd6b18fb4bd8287b766a06c4d1ec28f8785d61
George Guimarães <george.guimaraes@gmail.com>
2022-09-08 15:46:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Merge Nested Blocks into One
+
+Merge all nested blocks into one nested block with line breaks, as if it was written in document mode. 
+
+This is useful if you paste a long form written article and doesn't want it to behave as multiple bullet points.
+
+## Demo
+
+![demo](./demo.gif)
+
+## Icon 
+
+Icon by (Arrow Vectors by Vecteezy)[https://www.vecteezy.com/vector-art/6662126-consolidation-icon-vector-for-graphic-design-logo]
---
+++ index.ts
@@ -0,0 +1,34 @@
+import "@logseq/libs";
+
+async function merge_nested_blocks(blockId: string) {
+  const block = await logseq.Editor.getBlock(blockId, {
+    includeChildren: true
+  });
+  if (block === null || block.children?.length === 0) {
+    return;
+  }
+
+  const children = block.children as BlockEntity[];
+  let content = children.reduce(function (acc, block) { return acc + "\n\n" + block.content }, "");
+
+  await logseq.Editor.insertBlock(block.uuid, content, {
+    before: false
+  });
+
+  for (let child of children) {
+    await logseq.Editor.removeBlock(child.uuid);
+  };
+}
+
+
+logseq
+.ready(() => {
+  logseq.Editor.registerBlockContextMenuItem("Merge Nested Blocks Into One", async (e) => {
+    merge_nested_blocks(e.uuid);
+  });
+
+  logseq.Editor.registerSlashCommand("Merge Nested Blocks Into One", async (e) => {
+    merge_nested_blocks(e.uuid);
+  });
+})
+.catch(console.error);
