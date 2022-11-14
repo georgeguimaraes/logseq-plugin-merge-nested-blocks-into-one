91b05c9aa77357784be6cd50a9f8a800f6655eb7
George Guimarães <george.guimaraes@gmail.com>
2022-11-14 17:07:41 -0300

    fix: updating dependencies

--- index.ts
+++ index.ts
@@ -1,25 +1,47 @@
 import "@logseq/libs";
-
+// 
+// async function content_for_block_array(blocks: BlockEntity[]) {
+//   console.log("aaaa")
+//   let content = blocks.reduce(function (acc, block) {
+//     return acc + "\n\n" + content_for_block_and_children(block)
+//   }, "");
+//   console.log(content);
+//   return content;
+// }
+// 
+// async function content_for_block_and_children(block: BlockEntity) {
+//   if (block.children?.length === 0) {
+//     return block.content
+//   }
+//   else {
+//     let children = block.children as BlockEntity[];
+//     let content = children.reduce(function (acc, block) { return acc + "\n\n" + content_for_block_and_children(block) }, "");
+//     console.log("content for block and children");
+//     console.log(content);
+//     return content;
+//   }
+// }
+// 
 async function merge_nested_blocks(blockId: string) {
-  const block = await logseq.Editor.getBlock(blockId, {
+  const parent_block = await logseq.Editor.getBlock(blockId, {
     includeChildren: true
   });
   if (block === null || block.children?.length === 0) {
     return;
   }
 
-  const children = block.children as BlockEntity[];
-  let content = children.reduce(function (acc, block) { return acc + "\n\n" + block.content }, "");
+  const children = parent_block.children as BlockEntity[];
 
-  await logseq.Editor.insertBlock(block.uuid, content, {
+  let content =  "testing" //await content_for_block_array(children);
+
+  await logseq.Editor.insertBlock(parent_block.uuid, content, {
     before: false
   });
 
-  for (let child of children) {
-    await logseq.Editor.removeBlock(child.uuid);
-  };
+//   for (let child of children) {
+//     await logseq.Editor.removeBlock(child.uuid);
+//   };
 }
-
 
 logseq
 .ready(() => {
